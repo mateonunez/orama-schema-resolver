928fc8893b3b54152b257777e1b9fc642d8d0f12
mateonunez <mateonunez95@gmail.com>
2022-10-16 17:10:54 +0200

    test(schema-resolver): add simple tests with primitives

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,20 @@
 import resolveSchema from ".."
 import {test} from "tap"
 
-test("resolve", async ({test}) => {
-  test("should resolve", async ({same}) => {
-    same(resolveSchema({}), {name: "string"})
+test("resolve", ({test, plan}) => {
+  plan(2)
+
+  test("should resolve a simple schema using primitives", async ({same}) => {
+    const data = { name: "Lyra", stars: 5000, is_forked: true }
+    const schema = { name: "string", stars: "number", is_forked: "boolean" }
+
+    same(resolveSchema(data), schema)
+  })
+
+  test("shouln't resolve the reserved properties", async ({same}) => {
+    const data = { id: "123", name: "Lyra", stars: 5000, is_forked: true }
+    const schema = { name: "string", stars: "number", is_forked: "boolean" }
+
+    same(resolveSchema(data), schema)
   })
 })
