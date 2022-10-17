71d0a536831da8c6668bc1283f63a74acb005cab
mateonunez <mateonunez95@gmail.com>
2022-10-17 09:40:15 +0200

    test(schema-resolver): add array and error tests

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -2,7 +2,7 @@ import resolveSchema from ".."
 import {test} from "tap"
 
 test("resolve", ({test, plan}) => {
-  plan(4)
+  plan(6)
 
   test("should resolve a simple schema using primitives", async ({same}) => {
     const data = {name: "Lyra", stars: 5000, is_forked: true}
@@ -75,5 +75,69 @@ test("resolve", ({test, plan}) => {
     }
 
     same(resolveSchema(data), schema)
+  })
+
+  test("should resolve a schema with arrays", async ({same}) => {
+    const data = {
+      name: "Lyra",
+      stars: 5000,
+      is_forked: true,
+      owner: {
+        name: "Michele Riva",
+        age: 27,
+        is_active: true
+      },
+      sponsor: {
+        name: "NearForm",
+        website: "https://nearform.com"
+      },
+      contributors: [
+        {
+          name: "Paolo Insogna",
+          age: 30,
+          is_active: true
+        },
+        {
+          name: "Mateo Nunez",
+          age: 27,
+          is_active: true
+        },
+      ]
+    }
+
+    const schema = {
+      name: "string",
+      stars: "number",
+      is_forked: "boolean",
+      owner: {
+        name: "string",
+        age: "number",
+        is_active: "boolean"
+      },
+      sponsor: {
+        name: "string",
+        website: "string"
+      },
+      contributors: [
+        {
+          name: "string",
+          age: "number",
+          is_active: "boolean"
+        }
+      ]
+    }
+
+    same(resolveSchema(data), schema)
+  })
+
+  test("should throw an error if the data contains an unsupported type", async ({throws}) => {
+    const data = {
+      name: "Lyra",
+      stars: 5000,
+      is_forked: true,
+      unsupported: () => ({})
+    }
+
+    throws(() => resolveSchema(data), `Unsupported type ${typeof data.unsupported}`)
   })
 })
