0fac7e0b6c4ca1bf13c27c0427d0878c6c191adc
mateonunez <mateonunez95@gmail.com>
2022-10-17 09:40:03 +0200

    feat(schema-resolver): implements array support

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import {PropertiesSchema} from "@lyrasearch/lyra"
-import {ResolveSchema} from "@lyrasearch/lyra/dist/cjs/src/types"
-import {reservedPropertyNames} from "@lyrasearch/lyra/dist/cjs/src/utils"
+import { PropertiesSchema } from "@lyrasearch/lyra"
+import { ResolveSchema } from "@lyrasearch/lyra/dist/cjs/src/types"
+import { reservedPropertyNames } from "@lyrasearch/lyra/dist/cjs/src/utils"
 
-export default function resolveSchema<S extends ResolveSchema<any>>(data: S): PropertiesSchema {
+function computeGreatestElementIdx(arr: any[]): number {
+  const sizes = arr.map((el) => Object.values(el).length)
+  const greatesElement = sizes.reduce((max, v) => (max >= v ? max : v), -Infinity)
+  return sizes.indexOf(greatesElement)
+}
+
+export default function resolveSchema<S extends PropertiesSchema>(data: any): S & { [key: string]: ResolveSchema<typeof data> } {
   const schema: any = {}
+
+  if (Array.isArray(data)) {
+    const greatestElementIdx = computeGreatestElementIdx(data)
+    return resolveSchema(data[greatestElementIdx])
+  }
 
   for (const [key, value] of Object.entries(data)) {
     if (reservedPropertyNames.includes(key)) continue
@@ -16,8 +27,15 @@ export default function resolveSchema<S extends ResolveSchema<any>>(data: S): Pr
       schema[key] = "number"
     } else if (typeof value === "boolean") {
       schema[key] = "boolean"
+    } else if (Array.isArray(value)) {
+      if (!schema[key]) {
+        schema[key] = []
+        schema[key].push(resolveSchema(value))
+      }
     } else if (typeof value === "object") {
       schema[key] = resolveSchema(value)
+    } else {
+      throw new Error(`Unsupported type ${typeof value}`)
     }
   }
 
